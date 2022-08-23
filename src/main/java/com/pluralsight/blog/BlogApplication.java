6de3560282bb4595a2af7a2c55fc45a0248ef4ae
Lily <lilysdeng@gmail.com>
2022-08-23 13:12:43 -0400

    Module 2: Create the Post Repository and Save Data

--- src/main/java/com/pluralsight/blog/BlogApplication.java
+++ src/main/java/com/pluralsight/blog/BlogApplication.java
@@ -1,7 +1,11 @@
 package com.pluralsight.blog;
 
+import com.pluralsight.blog.model.Post;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+import java.util.List;
 
 @SpringBootApplication
 public class BlogApplication {
